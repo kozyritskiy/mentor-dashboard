8045fb3f75a3cbb12f14a685d8e0209231f62aad
Max Kozyritsky <maxim.kozyricki@gmail.com>
2019-02-23 15:23:02 +0300

    build: add buld folder

---
+++ build/precache-manifest.d8abd9672e90bfa76da2cc765f3575d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "fdd5dd75e358cb971064",
+    "url": "/static/js/main.fdd5dd75.chunk.js"
+  },
+  {
+    "revision": "d0f0a42fcbf3273333ab",
+    "url": "/static/js/2.d0f0a42f.chunk.js"
+  },
+  {
+    "revision": "fdd5dd75e358cb971064",
+    "url": "/static/css/main.a6eee049.chunk.css"
+  },
+  {
+    "revision": "6cc8a41e916e1660948e6aa74fa03a5d",
+    "url": "/index.html"
+  }
+];
